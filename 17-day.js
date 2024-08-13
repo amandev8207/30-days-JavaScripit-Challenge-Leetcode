f2252bce57d89627d50adb020421c7dbded2b43c
Aman <72264622+amandev8207@users.noreply.github.com>
2024-08-13 14:17:52 +0000

    add daily challenges

---
+++ 17-day.js
@@ -0,0 +1,54 @@
+// 2622. Cache With Time Limit
+
+// Write a class that allows getting and setting key-value pairs, however a time until expiration is associated with each key.
+
+// The class has three public methods:
+
+// set(key, value, duration): accepts an integer key, an integer value, and a duration in milliseconds. Once the duration has elapsed, the key should be inaccessible. The method should return true if the same un-expired key already exists and false otherwise. Both the value and duration should be overwritten if the key already exists.
+
+// get(key): if an un-expired key exists, it should return the associated value. Otherwise it should return -1.
+
+// count(): returns the count of un-expired keys.
+// var TimeLimitedCache = function() {
+    
+//     this.cache = new Map();
+// };
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const valcache = this.cache.get(key);
+    if (valcache) {
+      clearTimeout(valcache.timeout);
+    }
+    const timeout = setTimeout(() => this.cache.delete(key), duration);
+    this.cache.set(key, { value, timeout });
+    return Boolean(valcache);
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
